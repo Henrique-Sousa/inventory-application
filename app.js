84703b5dd9a1b6bbab59e58f51019da346b1c739
Henrique Sousa <sh4dowonth3w4ll@gmail.com>
2020-02-18 15:16:27 -0300

    add connection to mongoDB via mongoose

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://127.0.0.1/inventory';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
