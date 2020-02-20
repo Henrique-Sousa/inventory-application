59b6429f73dd6eee795bf2dc00dc2d955ad5f558
Henrique Sousa <sh4dowonth3w4ll@gmail.com>
2020-02-20 11:08:57 -0300

    first version of category list template

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,9 +2,6 @@ var Category = require('../models/category');
 
 exports.category_list = function(req, res) {
     Category.find({}, 'name', function(err, results){
-        for (let result of results) {
-            res.write(result.name + '\n');
-        }
-        res.end();
+       res.render('category_list', {title: 'Category list', results} );
     });
 };
