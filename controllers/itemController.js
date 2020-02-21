2bc1f1d5f55cb5ed03394cb742a6fa7131cddbfd
Henrique Sousa <sh4dowonth3w4ll@gmail.com>
2020-02-21 09:23:31 -0300

    define item_list and item_detail methods

---
+++ controllers/itemController.js
@@ -0,0 +1,12 @@
+var Item = require('../models/item')
+
+exports.item_list = function(req, res) {
+    Item.find().populate('category').exec(function(err, results){
+       res.render('item_list', {title: 'Item list', results} );
+    });
+};
+
+exports.item_detail = function(req, res) {
+    res.write('item ' + req.params.id);
+    res.end();
+};
