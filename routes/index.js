c6dc7d9fc0d71c812722775b4975ed068b6ff7e7
Henrique Sousa <sh4dowonth3w4ll@gmail.com>
2020-02-21 09:25:52 -0300

    add routes to item list and item detail

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var category_controller = require('../controllers/categoryController');
+var item_controller = require('../controllers/itemController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,5 +13,8 @@ router.get('/categories', category_controller.category_list);
 router.get('/category/create', category_controller.category_create_get);
 router.post('/category/create', category_controller.category_create_post);
 router.get('/category/:id', category_controller.category_detail);
+
+router.get('/items', item_controller.item_list);
+router.get('/item/:id', item_controller.item_detail);
 
 module.exports = router;
