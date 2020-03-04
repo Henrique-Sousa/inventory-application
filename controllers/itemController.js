8249c164b68573eb89db5f85eb4a7ba023d47f19
Henrique Sousa <sh4dowonth3w4ll@gmail.com>
2020-03-04 15:53:37 -0300

    implement update item on post

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -136,4 +136,53 @@ exports.item_update_get = function(req, res) {
     
 };
 
-exports.item_update_post = function(req, res) {res.send('...');};
+exports.item_update_post = [
+    // Convert category to an array.
+    (req, res, next) => {
+        if(!(req.body.category instanceof Array)){
+                if(typeof req.body.category==='undefined')
+                req.body.category=[];
+                else
+                req.body.category=new Array(req.body.category);
+            }
+        next();
+    },
+
+    // Validate fields.
+    body('item_name', 'Item name must not be empty.').isLength({ min: 1 }).trim(),
+    
+    // Sanitize fields (using wildcard).
+    // sanitizeBody('*').escape(),
+    // sanitizeBody('category.*').escape(),
+    //TODO não sei por que mas o array de categorias vira uma categoria so depois que eu uso escape()
+    
+    function(req, res, next) {
+
+        const errors = validationResult(req);
+        var item = new Item({
+            name: req.body.item_name,
+            description: req.body.item_description,
+            price: req.body.item_price,
+            number_in_stock: req.body.item_number_in_stock,
+            category: req.body.category,
+            _id: req.params.id
+        });
+        if(errors.isEmpty()) {
+            //TODO Verify if item already exists ???
+            Item.findByIdAndUpdate(req.params.id, item, {}, function(err, result) {
+                if (err) { return next(err); } 
+                res.redirect(result.url);
+            });
+        } else {
+            // There are errors. Render form again with sanitized values/error messages.
+            Category.find({}, 'name', function(err, all_categories) {
+                for(let i=0; i < all_categories.length; i++) {
+                    if (item.category.indexOf(all_categories[i]._id) > -1) {
+                        all_categories[i].checked = 'true';
+                    }
+                }
+                res.render('item_form', {title: 'Create item', item, categories: all_categories, errors: errors.array()} );
+            });
+        }
+    }
+];
